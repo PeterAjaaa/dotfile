cde574f09e19717d1f7746b3537bb711ba2257fc
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2023-03-18 01:21:40 +0700

    Remove 2 files

--- beautymode.sh
+++
@@ -1,7 +0,0 @@
-DIRECTORY=./i3-config-backup
-if [ ! -d "$DIRECTORY" ];
-then
-	mkdir $DIRECTORY
-fi
-cp ~/.config/i3/config ./i3-config-backup/config-performance
-cp ./i3-config-backup/config-beauty ~/.config/i3/config
--- performancemode.sh
+++
@@ -1,7 +0,0 @@
-DIRECTORY=./i3-config-backup
-if [ ! -d "$DIRECTORY" ];
-then
-	mkdir $DIRECTORY
-fi
-cp ~/.config/i3/config ./i3-config-backup/config-beauty
-cp ./i3-config-backup/config-performance ~/.config/i3/config
